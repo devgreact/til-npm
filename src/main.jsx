813e3ab8ddc87865e2f778e554524af0f5f50f3f
devgreact <dev.greact@gmail.com>
2025-01-10 13:19:54 +0900

    [docs] recoil 이해

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
-import store from "./store/store.js";
 
 createRoot(document.getElementById("root")).render(
   // 전연 store 를 활용함.
-  <Provider store={store}>
+  <RecoilRoot>
     <App />
-  </Provider>,
+  </RecoilRoot>,
 );
